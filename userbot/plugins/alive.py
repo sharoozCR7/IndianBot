43302ff4a1822d2a72b5daacb8c86146f20b4ad7
sharoozCR7 <57355970+sharoozCR7@users.noreply.github.com>
2020-06-22 13:39:56 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -8,13 +8,8 @@ from userbot.utils import admin_cmd
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-INDIANBOT_IS_ALIVE = ("**Apun Zinda He Sarr ^.^** \n`🇮🇳BOT Status : ` **☣Hot**\n\n"
-                     f"`My peru owner`: {DEFAULTUSER}\n\n"
-                     "`Indian Bot Version:` [1.0](https://telegra.ph/INDIAN-06-15-6)\n`Python:` **3.7.4**\n"
-                     "`Database Status:` **😀ALL OK**\n\n`Always with you, my master!\n`"
-                     "**Bot Creator:** [🇮🇳INDIAN BHAI](t.me/pureindialover)\n"
-                     "**Co-Owner:** [🇮🇳AKASH](t.me/AKASH_AM1)\n\n"
-                     "     [🇮🇳Deploy This IndianBot🇮🇳](https://github.com/indianbhaiya/IndianBot)") 
+INDIANBOT_IS_ALIVE = ("**Jeevanode und malare ^.^** \n`🇮🇳BOT Status : ` **☣Hot**\n\n"
+                     f"`My Muthalali`: {DEFAULTUSER}\n\n") 
 
 
 #@command(outgoing=True, pattern="^.alive$")
